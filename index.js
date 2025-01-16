15657901ef00c5d2b9390a188847043c356f8adc
sinha28shivam <sinha28.shivam@gmail.com>
2025-01-17 00:33:29 +0530

    Completed Authentication API

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import {connectDB} from './src/lib/db.js';
+import authRoute from './src/routes/auth.route.js';
+
+dotenv.config();
+const app = express();
+
+// Constants
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/v0/auth", authRoute);
+
+
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:', `https://localhost:${PORT}`);
+  connectDB();
+});

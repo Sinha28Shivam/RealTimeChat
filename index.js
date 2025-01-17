f18d1be928e6905db082bb4fe32c2328dfd95f50
sinha28shivam <sinha28.shivam@gmail.com>
2025-01-18 01:27:49 +0530

    Completed Send message and receive message functionality

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import {connectDB} from './src/lib/db.js';
+
 import authRoute from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js'
 
 dotenv.config();
 const app = express();
@@ -17,7 +19,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/v0/auth", authRoute);
-
+app.use("/api/v0/message", messageRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT:', `https://localhost:${PORT}`);
